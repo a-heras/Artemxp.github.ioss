52eddc1a4564ce1c89dd75007f03e3eee856a63d
Artemxp <artem.ice33@gmail.com>
2017-10-05 18:28:18 +0300

    fix

--- script.js
+++ script.js
@@ -21,16 +21,16 @@
 
     var p = new Popup({
       overlay: '.overlay',
-      popupModal: '.wrapper-modal',
+      popupModal: '.popup-modal',
     });
 
     document.querySelector('.button').onclick = function() {
-      var form = document.querySelector('.wrapper-modal');
+      var form = document.querySelector('form');
       p.open(form.innerHTML);
     };
 
     var input = document.querySelectorAll('.popup-form-input'),
-        form = document.querySelector('form');
+        form = document.querySelector('.popup-modal');
 
           form.onclick = function () {
               alert('1');

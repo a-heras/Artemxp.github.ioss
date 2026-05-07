989f2a83e95671027253237dedb4a351cc9a8c78
a-heras <32481400+a-heras@users.noreply.github.com>
2026-05-07 13:10:21 +0300

    Update script.js

--- bs/script.js
+++ bs/script.js
@@ -3,62 +3,40 @@ $(function () {
         var popup = this;
         popup.overlay = $(options.overlay);
         popup.popupModal = $(options.popupModal);
+
         this.open = function(content) {
             popup.popupModal.html(content);
-            popup.overlay.fadeIn(300);
-            popup.popupModal.fadeIn(300); 
-            resizeIframe();                
+            popup.overlay.fadeIn(200);
+            popup.popupModal.fadeIn(200);
+            resizeIframe();
         };
+
         this.close = function() {
             popup.overlay.fadeOut(200);
-            popup.popupModal.fadeOut(200);
-            setTimeout(function() {
+            popup.popupModal.fadeOut(200, function() {
                 popup.popupModal.empty();
-            }, 200);
+            });
         };
-        
-        popup.overlay.on('click', function() {
-            popup.close();
-        });
+
+        popup.overlay.on('click', popup.close);
     }
 
-    var p = new Popup({
-        overlay: '.overlay',
-        popupModal: '.popup-modal'
-    });
+    var p = new Popup({ overlay: '.overlay', popupModal: '.popup-modal' });
 
     $('.play').on('click', function() {
-        var videoUrl = $(this).attr('data-video');
+        var url = $(this).attr('data-video');
         var template = $('.wrapper-player').clone();
-        
-        if (videoUrl) {
-            var embedUrl = videoUrl.replace("watch?v=", "embed/");
-            template.find('iframe').attr('src', embedUrl);
-        }
-        
+        template.find('iframe').attr('src', url.replace("watch?v=", "embed/"));
         p.open(template.html());
     });
 
-    $('.button-call-me').on('click', function () {
-        var formHtml = $('.wrapper-call-me').html();
-        p.open(formHtml);
-    });
-
-    $('.button-price').on('click', function () {
-        var priceFormHtml = $('.wrapper-get-price').html();
-        p.open(priceFormHtml);
-    });
-
-    $(document).on('click', '.close img', function() {
-        p.close();
-    });
+    $('.button-call-me').on('click', function() { p.open($('.wrapper-call-me').html()); });
+    $('.button-price').on('click', function() { p.open($('.wrapper-get-price').html()); });
+    $(document).on('click', '.close img', p.close);
 
     function resizeIframe() {
-        $('.popup-modal iframe').each(function() {
-            var width = $(this).width();
-            $(this).css("height", (width / 1.7777) + "px");
-        });
+        var $if = $('.popup-modal iframe');
+        $if.css("height", ($if.width() / 1.77) + "px");
     }
-
     $(window).on('resize', resizeIframe);
 });

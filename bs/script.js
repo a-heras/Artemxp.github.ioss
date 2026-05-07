958681e9aeef4f48f1ca53840f2c67482eaaf06f
a-heras <32481400+a-heras@users.noreply.github.com>
2026-05-07 13:04:52 +0300

    Update script.js

--- bs/script.js
+++ bs/script.js
@@ -3,19 +3,20 @@ $(function () {
         var popup = this;
         popup.overlay = $(options.overlay);
         popup.popupModal = $(options.popupModal);
-
         this.open = function(content) {
             popup.popupModal.html(content);
-            popup.overlay.addClass('open');
-            popup.popupModal.addClass('open');
-            resizeIframe();
+            popup.overlay.fadeIn(300);
+            popup.popupModal.fadeIn(300); 
+            resizeIframe();                
         };
-
         this.close = function() {
-            popup.overlay.removeClass('open');
-            popup.popupModal.removeClass('open');
-            popup.popupModal.empty();
+            popup.overlay.fadeOut(200);
+            popup.popupModal.fadeOut(200);
+            setTimeout(function() {
+                popup.popupModal.empty();
+            }, 200);
         };
+        
         popup.overlay.on('click', function() {
             popup.close();
         });
@@ -28,16 +29,24 @@ $(function () {
 
     $('.play').on('click', function() {
         var videoUrl = $(this).attr('data-video');
-        var playerTemplate = $('.wrapper-player').clone();
+        var template = $('.wrapper-player').clone();
         
-        playerTemplate.find('iframe').attr('src', videoUrl);
+        if (videoUrl) {
+            var embedUrl = videoUrl.replace("watch?v=", "embed/");
+            template.find('iframe').attr('src', embedUrl);
+        }
         
-        p.open(playerTemplate.html());
+        p.open(template.html());
     });
 
     $('.button-call-me').on('click', function () {
         var formHtml = $('.wrapper-call-me').html();
         p.open(formHtml);
+    });
+
+    $('.button-price').on('click', function () {
+        var priceFormHtml = $('.wrapper-get-price').html();
+        p.open(priceFormHtml);
     });
 
     $(document).on('click', '.close img', function() {
